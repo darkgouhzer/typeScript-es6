2ceab9b0df0f1dfb6c5dd5062396ebe3178c6c15
darkgouhzer <30606397+darkgouhzer@users.noreply.github.com>
2018-01-09 12:50:39 -0700

    Funciones de Flecha
    practica de funciones de flecha

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 "use strict";
-var nombre = "Hugo";
-var apellido = "Bojorquez";
-var edad = 31;
-var texto = "Hola, " + nombre + " " + apellido + " " + edad;
-console.log(texto);
+var miFuncion = function (a) {
+    return a;
+};
+var miFuncionF = function (a) { return a; };
+var miFuncion2 = function (a, b) {
+    return a + b;
+};
+var mifuncion2F = function (a, b) { return a + b; };
+var miFuncion3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var mifuncion3F = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var nombre = "pedro";
+var hulk = {
+    nombre: "Bruce",
+    smash: function () {
+        var _this = this;
+        setTimeout(function () { return console.log(_this.nombre + " smash!!"); }, 1500);
+    }
+};
+hulk.smash();
+console.log(miFuncion("Normal"));
+console.log(miFuncionF("Flecha"));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,7 +1,34 @@
-let nombre ="Hugo";
-let apellido="Bojorquez";
-let edad=31;
+let miFuncion =function( a ){
+  return a;
+}
 
-let texto = `Hola, ${nombre} ${apellido} ${edad}`;
+let miFuncionF = a => a;
+let miFuncion2 = function(a:number, b:number)
+{
+  return a+b;
+}
 
-console.log(texto);
+let mifuncion2F = (a:number,b:number) => a+b;
+
+let miFuncion3 = function(nombre:string)
+{
+  nombre = nombre.toUpperCase();
+  return nombre;
+}
+
+let mifuncion3F = (nombre:string) =>{
+  nombre = nombre.toUpperCase();
+  return nombre;
+}
+
+let nombre  = "pedro"
+let hulk = {
+  nombre: "Bruce",
+  smash(){
+    setTimeout(()=> console.log(this.nombre+ " smash!!"), 1500);
+  }
+}
+
+hulk.smash();
+console.log(miFuncion("Normal"));
+console.log(miFuncionF("Flecha"));
