b100bfcb32ea1cdabff02c383ec1d0011656106b
darkgouhzer <30606397+darkgouhzer@users.noreply.github.com>
2018-01-09 22:57:42 -0700

    Practica de Objetos y arreglos

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
 "use strict";
-<<<<<<< HEAD
-function activar(quien, objeto, momento) {
-    if (objeto === void 0) { objeto = "batiseñal"; }
-    var mensaje;
-    if (momento) {
-        mensaje = quien + " activ\u00F3 la " + objeto + " en la " + momento;
-    }
-    else {
-        mensaje = quien + " activ\u00F3 la " + objeto;
-    }
-    console.log(mensaje);
-}
-activar("Gordon", "batiseñal", "tarde");
-=======
-var miFuncion = function (a) {
-    return a;
-};
-var miFuncionF = function (a) { return a; };
-var miFuncion2 = function (a, b) {
-    return a + b;
-};
-var mifuncion2F = function (a, b) { return a + b; };
-var miFuncion3 = function (nombre) {
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-var mifuncion3F = function (nombre) {
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-var nombre = "pedro";
-var hulk = {
-    nombre: "Bruce",
-    smash: function () {
-        var _this = this;
-        setTimeout(function () { return console.log(_this.nombre + " smash!!"); }, 1500);
-    }
-};
-hulk.smash();
-console.log(miFuncion("Normal"));
-console.log(miFuncionF("Flecha"));
->>>>>>> 2ceab9b0df0f1dfb6c5dd5062396ebe3178c6c15
+// let avenger = {
+//   nombre:"Steve",
+//   clave:"Capitan America",
+//   poder: "Droga"
+// }
+//
+// let { nombre, clave, poder } = avenger;
+//
+// // let nombre =avenger.nombre;
+// // let clave = avenger.clave;
+// // let poder = avenger.poder;
+//
+// console.log(nombre, clave, poder);
+var avengers = ["Thor", "Steve", "Tony"];
+var ironman = avengers[2];
+console.log(ironman);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,52 +1,21 @@
-<<<<<<< HEAD
-function activar( quien:string, objeto:string ="batiseñal", momento?:string){
-
-  let mensaje:string;
-  if(momento)
-  {
-    mensaje = `${ quien } activó la ${ objeto } en la ${ momento }`;
-  }else{
-    mensaje = `${ quien } activó la ${ objeto }`;
-  }
-
-  console.log(mensaje);
-}
 
 
-activar("Gordon", "batiseñal", "tarde");
-=======
-let miFuncion =function( a ){
-  return a;
-}
+// let avenger = {
+//   nombre:"Steve",
+//   clave:"Capitan America",
+//   poder: "Droga"
+// }
+//
+// let { nombre, clave, poder } = avenger;
+//
+// // let nombre =avenger.nombre;
+// // let clave = avenger.clave;
+// // let poder = avenger.poder;
+//
+// console.log(nombre, clave, poder);
 
-let miFuncionF = a => a;
-let miFuncion2 = function(a:number, b:number)
-{
-  return a+b;
-}
+let avengers:string[] = ["Thor", "Steve", "Tony"];
 
-let mifuncion2F = (a:number,b:number) => a+b;
+let [, , ironman] = avengers;
 
-let miFuncion3 = function(nombre:string)
-{
-  nombre = nombre.toUpperCase();
-  return nombre;
-}
-
-let mifuncion3F = (nombre:string) =>{
-  nombre = nombre.toUpperCase();
-  return nombre;
-}
-
-let nombre  = "pedro"
-let hulk = {
-  nombre: "Bruce",
-  smash(){
-    setTimeout(()=> console.log(this.nombre+ " smash!!"), 1500);
-  }
-}
-
-hulk.smash();
-console.log(miFuncion("Normal"));
-console.log(miFuncionF("Flecha"));
->>>>>>> 2ceab9b0df0f1dfb6c5dd5062396ebe3178c6c15
+console.log( ironman)
